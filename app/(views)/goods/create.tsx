7ce7b6af7450be71ee14df1e3cec9b59a9a2338b
ShetePro <sheteprolin@gmail.com>
2024-12-11 10:59:52 +0800

    feat(app): goods create 接受brand 品牌参数

--- app/(views)/goods/create.tsx
+++ app/(views)/goods/create.tsx
@@ -1,10 +1,12 @@
 import PageView from "@/components/PageView";
 import CreateGoods from "@/components/goods/CreateGoods";
+import { useLocalSearchParams } from "expo-router";
 
-export default function GoodsCreatePage () {
+export default function GoodsCreatePage() {
+  const { brand } = useLocalSearchParams();
   return (
     <PageView>
-      <CreateGoods></CreateGoods>
+      <CreateGoods brand={brand}></CreateGoods>
     </PageView>
-  )
+  );
 }
